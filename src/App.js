d551349c8c6323553df10a213e57be83d6d98498
Azzidahaka <Azzidahaka@gmail.com>
2024-05-04 02:09:26 -0600

    updated to test integration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
+import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents } from './api';
 
 import './App.css';
-import NumberOfEvents from './components/NumberOfEvents';
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className='App'>
       <NumberOfEvents />
       <CitySearch />
-      <EventList />
+      <EventList events={events} />
     </div>
   );
 };
