4e24be2f82456af518b14fa546839e3c74c21ef1
Azzidahaka <Azzidahaka@gmail.com>
2024-05-04 19:02:21 -0600

    updated access token endpoint

--- src/api.js
+++ src/api.js
@@ -29,7 +29,7 @@ const getToken = async (code) => {
   try {
     const encodeCode = encodeURIComponent(code);
 
-    const response = await fetch('YOUR_GET_ACCESS_TOKEN_ENDPOINT' + '/' + encodeCode);
+    const response = await fetch('api/token' + '/' + encodeCode);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
