e30f1ef981973420d072acfb3331cebb12c5527e
Azzidahaka <Azzidahaka@gmail.com>
2024-05-25 05:20:46 -0600

    added pie and scatter charts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
 import { useCallback, useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
@@ -54,6 +56,14 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
+      <div className='charts-container'>
+        <CityEventsChart
+          allLocations={allLocations}
+          events={events}
+        />
+        <EventGenresChart events={events} />
+      </div>
+
       <EventList
         events={events}
         currentNOE={currentNOE}
